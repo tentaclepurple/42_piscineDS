c23d819d401ea7e673bac09846c3311123e62c44
Iban Montero Martinez <imontero@c4r1s5.42urduliz.com>
2024-08-20 19:54:14 +0200

    ok

--- 2-Dataanalyst/ex03/Building.py
+++ 2-Dataanalyst/ex03/Building.py
@@ -1,10 +1,10 @@
 from sqlalchemy import create_engine
+import numpy as np
 import pandas as pd
 from dotenv import load_dotenv
 import os
 import matplotlib.pyplot as plt
-import seaborn as sns
-from tqdm import tqdm
+import matplotlib.ticker as ticker
 import time
 
 
@@ -21,120 +21,153 @@ DATABASE_URL = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NA
 engine = create_engine(DATABASE_URL)
 
 query = """
-WITH pedidos_por_cliente AS (
-    SELECT user_id, COUNT(DISTINCT user_session) AS num_pedidos
+WITH orders_by_client AS (
+    SELECT user_id, COUNT(DISTINCT user_session) AS order_count
+
     FROM (
         SELECT DISTINCT user_id, user_session
-        FROM customers
+        FROM customers2
         WHERE event_type = 'purchase'
     ) AS pedidos_unicos
     GROUP BY user_id
 ),
-todos_los_clientes AS (
+all_clients AS (
     SELECT DISTINCT user_id
-    FROM customers
+    FROM customers2
 ),
-clientes_con_pedidos AS (
+clients_with_orders AS (
     SELECT 
         CASE
-            WHEN num_pedidos IS NULL THEN '0 pedidos'
-            WHEN num_pedidos BETWEEN 1 AND 8 THEN '1 - 8 pedidos'
-            WHEN num_pedidos BETWEEN 9 AND 16 THEN '9 - 16 pedidos'
-            WHEN num_pedidos BETWEEN 17 AND 24 THEN '17 - 24 pedidos'
-            WHEN num_pedidos BETWEEN 25 AND 31 THEN '25 - 31 pedidos'
-            WHEN num_pedidos BETWEEN 32 AND 38 THEN '32 - 38 pedidos'
-            ELSE '39+ pedidos'
-        END AS rango_pedidos
-    FROM todos_los_clientes
-    LEFT JOIN pedidos_por_cliente USING (user_id)
+            WHEN order_count
+             IS NULL THEN '0'
+            WHEN order_count
+             BETWEEN 1 AND 8 THEN '1 - 8'
+            WHEN order_count
+             BETWEEN 9 AND 16 THEN '9 - 16'
+            WHEN order_count
+             BETWEEN 17 AND 23 THEN '17 - 24'
+            WHEN order_count
+             BETWEEN 24 AND 31 THEN '25 - 31'
+            ELSE '32+'
+        END AS order_range
+    FROM all_clients
+    LEFT JOIN orders_by_client USING (user_id)
 )
 SELECT 
-    rango_pedidos,
-    COUNT(*) AS num_clientes
-FROM clientes_con_pedidos
-GROUP BY rango_pedidos
+    order_range,
+    COUNT(*) AS client_number
+FROM clients_with_orders
+GROUP BY order_range
 ORDER BY 
-    CASE rango_pedidos
-        WHEN '0 pedidos' THEN 1
-        WHEN '1 - 8 pedidos' THEN 2
-        WHEN '9 - 16 pedidos' THEN 3
-        WHEN '17 - 24 pedidos' THEN 4
-        WHEN '25 - 31 pedidos' THEN 5
-        WHEN '32 - 38 pedidos' THEN 6
-        ELSE 7
+    CASE order_range
+        WHEN '0' THEN 1
+        WHEN '1 - 8' THEN 2
+        WHEN '9 - 16' THEN 3
+        WHEN '17 - 24' THEN 4
+        WHEN '25 - 31' THEN 5
+        ELSE 6
     END;
 """
 
-total_rows_query = """
-SELECT COUNT(*)
-FROM
-    customers
-WHERE
-    event_type = 'purchase'
+query2 = """
+SELECT 
+    CASE 
+        WHEN total_amount > 0 AND total_amount <= 50 THEN 0
+        WHEN total_amount > 50 AND total_amount <= 100 THEN 50
+        WHEN total_amount > 100 AND total_amount <= 150 THEN 100
+        WHEN total_amount > 150 AND total_amount <= 200 THEN 150
+        ELSE 200
+    END AS amount_spent,
+    COUNT(*) AS client_amount
+FROM 
+    (
+    SELECT 
+        user_id, 
+        SUM(price) AS total_amount
+    FROM 
+        customers2
+    WHERE 
+        event_type = 'purchase'
+    GROUP BY 
+        user_id
+    ) AS buys_per_user
+GROUP BY 
+    amount_spent
+ORDER BY 
+    amount_spent;
 """
 
 
-def get_df():
-    chunks = []
-    
-    total_rows = pd.read_sql(total_rows_query, engine).iloc[0, 0]
-    
-    for chunk in tqdm(pd.read_sql(query, engine, chunksize=10000), total=total_rows//10000, unit='chunk'):
-        chunks.append(chunk)
-    df = pd.concat(chunks, ignore_index=True)
-    return df
-
-
-def describe_and_box_plots(df):
+def buildings(df, df2):
     """
     Print the description of the DataFrame and save boxplots.
     """
-    pd.set_option('display.float_format', '{:.2f}'.format)
-    print(df.describe())
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 8))
 
+    df_filtered = df[df['order_range'] != '0']
+
+    positions = np.linspace(4, 35.5, len(df_filtered))
+
+    bars = ax1.bar(positions, df_filtered['client_number'], width=(35.5 - 4) / len(df_filtered) * 0.8, align='center')
+
+    ax1.set_title('Number of Clients by Order Range (Excluding 0 orders)')
+    ax1.set_xlabel('frequency')
+    ax1.set_ylabel('customers (log scale)')
+
+    ax1.set_yscale('log')
+    ax1.yaxis.set_major_formatter(ticker.FuncFormatter(lambda y, _: f'{int(y):,}'))
+
+    ax1.set_xticks([0, 10, 20, 30])
+    ax1.set_xticklabels(['0', '10', '20', '30'])
+
+    ax1.grid(axis='both', linestyle='--', alpha=0.7)
+
+    ax1.spines['top'].set_visible(False)
+    ax1.spines['right'].set_visible(False)
+
+    for bar in bars:
+        height = bar.get_height()
+        ax1.text(bar.get_x() + bar.get_width()/2., height,
+                f'{int(height)}', ha='center', va='bottom')
+
+
+    x_labels = df2['amount_spent'].astype(str)
+
+    bar_width = 0.8
     
-    # Consulta SQL modificada para obtener los datos necesarios
+    x_labels = ['0-50', '50-100', '100-150', '150-200', '200+']
+
+    x_positions = range(len(x_labels))
+
+    ax2.bar(x_positions, df2['client_amount'], color='skyblue', width=bar_width)
 
 
-    # Ejecuta la consulta y carga los resultados en un DataFrame
-    df = pd.read_sql(query, engine)
-
-    # Crear figura con dos subplots
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 8))
-
-    # Gráfico 1: Número de pedidos según la frecuencia
-    order_counts = df['order_count'].value_counts().sort_index()
-    sns.barplot(x=order_counts.index, y=order_counts.values, ax=ax1)
-    ax1.set_title('Number of Orders According to Frequency')
-    ax1.set_xlabel('Frequency')
-    ax1.set_ylabel('Customers')
-
-    # Gráfico 2: Dólares Altairianos gastados en el sitio
-    # Agrupamos los gastos en intervalos
-    df['spend_group'] = pd.cut(df['total_spent'], bins=[0, 50, 100, 150, 200, float('inf')],
-                            labels=['0-50', '50-100', '100-150', '150-200', '200+'])
-    spend_counts = df['spend_group'].value_counts().sort_index()
-    sns.barplot(x=spend_counts.index, y=spend_counts.values, ax=ax2)
-    ax2.set_title('Altairian Dollars Spent on the Site by Customers')
-    ax2.set_xlabel('Monetary Value in ₳')
-    ax2.set_ylabel('Customers')
+    ax2.set_ylabel('customers')
+    ax2.set_xlabel('monetary value in Altairians')
+    ax2.set_title('Number of Clients by Monetary Value')
+    
+    ax2.set_xticks(x_positions)
+    ax2.set_xticklabels(x_labels)
 
     plt.tight_layout()
-    plt.savefig('boxplot_1.png')
-    plt.close()   
+
+    plt.savefig('bar_chart_buildings.png')
+    plt.close()
 
 
 if __name__ == "__main__":
     start_time = time.time()
 
-    try:
-        #df = pd.read_sql(query, engine)
-        df = get_df()
-        print(df)
+    #try:
+    df = pd.read_sql(query, engine)
+    df2 = pd.read_sql(query2, engine)
+    print("df 1\n", df)
+    print()
+    print("df 2\n", df2)
+    buildings(df, df2)
 
-        describe_and_box_plots(df)
         
-    except Exception as error:
+"""     except Exception as error:
         print(f"An error occurred: {error}")
 
-    print("Execution time:", time.time() - start_time)
+    print("Execution time:", time.time() - start_time) """
