cf4c517c4921218eaa27e35073ced9d5c9a44cd9
Iban Montero Martinez <imontero@c4r4s4.42urduliz.com>
2024-08-28 20:23:57 +0200

    update elbow.py

--- 2-Dataanalyst/ex04/elbow2.py
+++ 2-Dataanalyst/ex04/elbow2.py
@@ -3,7 +3,6 @@ import pandas as pd
 from dotenv import load_dotenv
 import os
 import matplotlib.pyplot as plt
-import matplotlib.ticker as mticker
 import time
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
@@ -28,7 +27,6 @@ SELECT user_id, COUNT(*) AS purchases
 FROM customers2
 WHERE event_type = 'purchase'
 GROUP BY user_id
-HAVING COUNT(*) < 30
 ORDER BY purchases DESC;
 """
 
@@ -38,18 +36,60 @@ def ft_clusters(df):
     Calculate and plot clusters and inertia
     '''
     
+    pd.set_option('display.float_format', lambda x: '%.2f' % x)
+
+    print(f"DF base: \n {df}")
+    print()
+    print(f"DF base describe: \n {df.describe()}")
+    print()
+    
+    scaler = StandardScaler()
+    df_scaled = scaler.fit_transform(df[['purchases']])
+
     wss = []
     for k in range(1, 11):
-        kmeans = KMeans(n_clusters=k, random_state=0, n_init=10).fit(df)
+        kmeans = KMeans(n_clusters=k, random_state=0, n_init=10).fit(df_scaled)
         wss.append(kmeans.inertia_)
-
+    
     plt.plot(range(1, 11), wss)
     plt.xlabel("Number of clusters")
     plt.title("The Elbow Method")
-
     plt.tight_layout()
+    plt.savefig('elbow2.png')
+    plt.close()
 
-    plt.savefig('elbowB.png')
+    kmeans = KMeans(n_clusters=2, random_state=0)
+    clusters = kmeans.fit_predict(df_scaled)
+    df['2clusters'] = clusters
+    
+    cluster_summary = df.groupby('2clusters').describe()
+    print(cluster_summary['purchases'].T)
+    print()
+    
+    kmeans_3 = KMeans(n_clusters=3, random_state=0)
+    clusters_3 = kmeans_3.fit_predict(df_scaled)
+    df['3clusters'] = clusters_3
+    
+    cluster_summary_3 = df.groupby('3clusters').describe()
+    print(cluster_summary_3['purchases'].T)
+    print()
+
+    ax = df[df['2clusters'] == 0].boxplot(column='purchases', grid=False, vert=False, showfliers=False)
+    plt.title('Distribución de Compras - Cluster 0')
+    plt.xlabel('Cluster 0')
+    plt.ylabel('')
+    ax.set_yticklabels([])
+    plt.tight_layout()
+    plt.savefig('2_clusters_boxplot_cluster_0.png')
+    plt.close()
+    
+    ax = df[df['2clusters'] == 1].boxplot(column='purchases', grid=False, vert=False, showfliers=False)
+    plt.title('Distribución de Compras - Cluster 1')
+    plt.xlabel('Cluster 1')
+    plt.ylabel('')
+    ax.set_yticklabels([])
+    plt.tight_layout()
+    plt.savefig('2_clusters_boxplot_cluster_1.png')
     plt.close()
     
     
